d38c5ecd7f82e4fd1053f78765638f5f7b8e9477
breeze0 <1340287826@163.com>
2018-08-14 16:57:16 +0800

    Can get tree's number of leafs and depth

--- treePlotter.py
+++ treePlotter.py
@@ -1,7 +1,5 @@
 # -*- coding: UTF-8 -*-
-import sys
-reload(sys)
-sys.setdefaultencoding("utf-8")
+
 import matplotlib.pyplot as plt
 #定义文本框和箭头格式
 decisionNode = dict(boxstyle="sawtooth", fc="0.8")
@@ -20,3 +18,24 @@ def createPlot():
     plotNode(s1, (0.5, 0.1), (0.1, 0.5), decisionNode)
     plotNode(s2, (0.8, 0.1), (0.3, 0.8), leafNode)
     plt.show()
+#获取叶节点的数目
+def getNumberLeafs(myTree):
+    numLeafs = 0
+    firstStr = myTree.keys()[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__=='dict':
+            numLeafs += getNumberLeafs(secondDict[key])
+        else:   numLeafs += 1
+    return numLeafs
+#获取树的层数
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstStr = myTree.keys()[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__=='dict':
+            thisDepth = 1 + getTreeDepth(secondDict[key])
+        else:   thisDepth = 1
+        if thisDepth > maxDepth: maxDepth = thisDepth
+    return maxDepth
