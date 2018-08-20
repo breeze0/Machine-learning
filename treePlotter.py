8ec9364fb9f871012961fb96accb0af887b3ab08
breeze0 <1340287826@163.com>
2018-08-20 11:07:50 +0800

    Plot tree

--- treePlotter.py
+++ treePlotter.py
@@ -9,14 +9,16 @@ arrow_args = dict(arrowstyle="<-")
 def plotNode(nodeTxt, centerPt, parentPt,nodeType):
     createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction',xytext=centerPt, textcoords='axes fraction',va="center",ha="center",bbox=nodeType,arrowprops=arrow_args)
 
-def createPlot():
+def createPlot(inTree):
     fig = plt.figure(1, facecolor='white')
     fig.clf()
+    #axprops = dict(xticks[], yticks[])
     createPlot.ax1 = plt.subplot(111, frameon=False)
-    s1 = 'decision node'
-    s2 = 'leaf node'
-    plotNode(s1, (0.5, 0.1), (0.1, 0.5), decisionNode)
-    plotNode(s2, (0.8, 0.1), (0.3, 0.8), leafNode)
+    plotTree.totalW = float(getNumberLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xoff = -0.5/plotTree.totalW
+    plotTree.yoff = 1.0
+    plotTree(inTree, (0.5,1.0), '')
     plt.show()
 #获取叶节点的数目
 def getNumberLeafs(myTree):
@@ -39,3 +41,29 @@ def getTreeDepth(myTree):
         else:   thisDepth = 1
         if thisDepth > maxDepth: maxDepth = thisDepth
     return maxDepth
+def retrieveTree(i):
+    listOfTrees = [{'no facing': {0: 'no',1:{'flippers': {0:'no',1:'yes'}}}},{'no facing':{0:'no',1:{'flippers': {0:{'head':{0:'no',1:'yes'}},1:'no'}}}}]
+    return listOfTrees[i]
+#父子结点间填充文本信息
+def plotMidTxt(cntrPt, parentPt, txtString):
+    xMid = (parentPt[0] - cntrPt[0])/2.0 + cntrPt[0]
+    yMid = (parentPt[1] - cntrPt[1])/2.0 + cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString)
+
+def plotTree(myTree, parentPt, nodeTxt):
+    numLeafs = getNumberLeafs(myTree)
+    depth = getTreeDepth(myTree)
+    firstStr = myTree.keys()[0]
+    cntrPt = (plotTree.xoff + (1.0 + float(numLeafs))/2.0/plotTree.totalW, plotTree.yoff)
+    plotMidTxt(cntrPt, parentPt, nodeTxt)
+    plotNode(firstStr, cntrPt, parentPt, decisionNode)
+    secondDict = myTree[firstStr]
+    plotTree.yoff = plotTree.yoff - 1.0/plotTree.totalD
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ =='dict':
+            plotTree(secondDict[key], cntrPt, str(key))
+        else:
+            plotTree.xoff = plotTree.xoff + 1.0/plotTree.totalW
+            plotNode(secondDict[key], (plotTree.xoff, plotTree.yoff), cntrPt, leafNode)
+            plotMidTxt((plotTree.xoff, plotTree.yoff), cntrPt, str(key))
+    plotTree.yoff = plotTree.yoff + 1.0/plotTree.totalD
